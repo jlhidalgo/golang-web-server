16b6a13fdbb6b116557f178832efdbdfcdbfb900
Juan Luis Hidalgo <juanluis.hidalgo@gmail.com>
2022-12-19 22:47:36 -0800

    add default handlers and listener

--- golang-web-server.go
+++ golang-web-server.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"html"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Testing")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi")
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
