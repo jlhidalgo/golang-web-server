1e2c6b0e87405701da99fddbd44e453ba9a0666b
Juan Luis Hidalgo <juanluis.hidalgo@gmail.com>
2022-12-20 20:52:10 -0800

    add hiString method

--- golang-web-server.go
+++ golang-web-server.go
@@ -10,13 +10,15 @@ func echoString(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello")
 }
 
+func hiString(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi")
+}
+
 func main() {
 
 	http.HandleFunc("/", echoString)
 
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi")
-	})
+	http.HandleFunc("/hi", hiString)
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
