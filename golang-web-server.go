a578adc7e1da1c782390698b0f395c23ca8afb50
Juan Luis Hidalgo <juanluis.hidalgo@gmail.com>
2023-01-05 21:37:21 -0800

    add usage information and mechanism to choose option

--- golang-web-server.go
+++ golang-web-server.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"sync"
 )
 
 var counter int
 var mutex = &sync.Mutex{}
+var valid_args = []string{"handler_functions", "handler_directory"}
 
 func serveFile(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, r.URL.Path[1:])
@@ -36,8 +38,37 @@ func initializeHandler() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 }
 
+func printUsage() {
+	fmt.Println("Usage: ./golang-web-server option")
+	fmt.Println("  options:")
+	fmt.Println("    handler_functions")
+	fmt.Println("    handler_directory")
+	fmt.Println("  example:")
+	fmt.Println("./golang-web-server handler_directory")
+}
+
+func contains(options []string, str string) bool {
+	for _, option := range options {
+		if option == str {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
-	//initializeHandlerFunctions()
-	initializeHandler()
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	args := os.Args[1:]
+	if len(args) <= 0 || !contains(valid_args, args[0]) {
+		printUsage()
+	} else {
+
+		switch args[0] {
+		case "handler_functions":
+			initializeHandlerFunctions()
+		case "handler_directory":
+			initializeHandler()
+		}
+
+		log.Fatal(http.ListenAndServe(":8081", nil))
+	}
 }
