53e12dc2f65e7a67c1da582ae3901d777039da18
Juan Luis Hidalgo <juanluis.hidalgo@gmail.com>
2023-01-28 12:49:26 -0800

    update main function to use mhttp package

--- cmd/web-server/golang-web-server.go
+++ cmd/web-server/golang-web-server.go
@@ -2,27 +2,29 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"log"
-	"net/http"
 
-	"github.com/jlhidalgo/golang-web-server/pkg/handler"
+	"github.com/jlhidalgo/golang-web-server/pkg/mhttp"
 )
 
-const staticDirectory string = "./web/static"
+const (
+	staticDirectory string = "./web/static"
+	url             string = "localhost"
+	port            string = "8081"
+)
 
 var useHandlerFunctions bool
 
 func main() {
-	flag.BoolVar(&useHandlerFunctions, "use-handler-functions", true, "Runs the Web Browser with handler functions")
+	flag.BoolVar(&useHandlerFunctions, "use-handler-functions", true, "Runs the Web Browser with handler functions, use false to run it in FileServer mode.")
 	flag.Parse()
 
-	if useHandlerFunctions {
-		handler.InitializeHandlerFunctions(staticDirectory)
-	} else {
-		handler.InitializeHandler(staticDirectory)
-	}
+	server := mhttp.NewServer(staticDirectory, url, port)
 
-	fmt.Println("Listening on localhost:8081...")
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	if useHandlerFunctions {
+		server.InitializeHandlerFunctions()
+	} else {
+		server.InitializeFileServer()
+	}
+	server.ListenAndServe()
+
 }
